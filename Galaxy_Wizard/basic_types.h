48b93f80888fe71064b8efc47cbfd4da555d7ee2
Козлов Сергей Александрович <kozlov_sergey_aleksandrovitch@mail.ru>
2020-05-08 01:47:39 +0300

    Вернулись к проверенным ценностям фигур

--- Galaxy_Wizard/basic_types.h
+++ Galaxy_Wizard/basic_types.h
@@ -6,11 +6,11 @@
 typedef int64_t DWORD;
 
 const DWORD Pawn_Value = 200;
-const DWORD King_Value = 7 * Pawn_Value;
-const DWORD Queen_Value = 13 * Pawn_Value;
-const DWORD Rook_Value = 7 * Pawn_Value;
-const DWORD Bishop_Value = 4 * Pawn_Value;
-const DWORD Knight_Value = 3 * Pawn_Value;
+const DWORD King_Value = 500 * Pawn_Value;
+const DWORD Queen_Value = 11 * Pawn_Value;
+const DWORD Rook_Value = 6 * Pawn_Value;
+const DWORD Bishop_Value = DWORD(4.25 * Pawn_Value);
+const DWORD Knight_Value = 4 * Pawn_Value;
 
 const DWORD MoveScoreBonus = Pawn_Value / 50;
 
